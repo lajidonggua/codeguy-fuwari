cbd1201f2b675fbb16aa34320defdb1de69e46e2
Yummy <yummy.dp.yang@gtomato.com>
2026-02-27 15:50:34 +0800

    add cloudflare deploy

--- src/config.ts
+++ src/config.ts
@@ -54,7 +54,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar2.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "冬瓜",
+	name: "lajidonggua",
 	bio: "A code guy who loves blogging",
 	links: [
 		// {
