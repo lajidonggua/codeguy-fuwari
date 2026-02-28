b0ec05d8d8d226d4d5e49316a69665b74ca5edde
lajidonggua <dongguaxiami@gmail.com>
2026-02-28 11:31:39 +0800

    fix

--- src/config.ts
+++ src/config.ts
@@ -27,14 +27,14 @@ export const siteConfig: SiteConfig = {
 	},
 	toc: {
 		enable: true, // Display the table of contents on the right side of the post
-		depth: 3, // Maximum heading depth to show in the table, from 1 to 3
+		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
 	},
 	favicon: [
 		// Leave this array empty to use the default favicon
 		{
 			src: "/favicon/favicon.png", // Path of the favicon, relative to the /public directory
 			theme: "light", // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-			sizes: "35x35", // (Optional) Size of the favicon, set only if you have favicons of different sizes
+			sizes: "32x32", // (Optional) Size of the favicon, set only if you have favicons of different sizes
 		},
 	],
 };
